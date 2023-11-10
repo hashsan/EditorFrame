5562baf1bbddddfbeae32357a84f4abbd4c48d55
はっしゅさん <149641384+hashsan@users.noreply.github.com>
2023-11-10 14:28:11 +0900

    Update EditorFrame.js

--- EditorFrame.js
+++ EditorFrame.js
@@ -73,3 +73,88 @@ ed.setMessage('yyyyy')
 document.body.append(ed.frame)
 
 */
+
+
+
+
+
+
+
+//Debounce 関数の例
+const debounce = (func, timeout) => {
+  let timer;
+  // 引数に受け取った関数 func を拡張して返す
+  return function (...args) {
+    clearTimeout(timer);
+    // timeout で指定された時間後に呼び出しをスケジュール
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  }
+}
+
+export class Press{
+  el_press;
+  ary = []
+  constructor(el){
+    if(!el){
+      throw new Error('element need it!')
+    }
+    this.el_press = el
+    this.el_press.addEventListener('keydown',this.onkeydown)
+  }
+  press=(key,cb,time)=>{
+    key = key.replaceAll(' ','')
+    if(time){
+      this.ary.push({ key, cb:debounce(cb,time) })
+      return this
+    }
+    this.ary.push({ key, cb })
+    return this
+  }
+  onkeydown = (e) =>{
+    var ch =''
+    if(e.ctrlKey){
+      ch +='ctrl+'
+    }
+    if(e.shiftKey){
+      ch +='shift+'
+    }
+    if(e.altKey){
+      ch +='alt+'
+    }
+    ch+=e.key
+    const caller = this.get(ch)
+    if(!caller){
+      return
+    }
+    caller.cb(e)    
+  }
+  get = (ch) =>{    
+    let caller = this.ary.filter(d=>d.key===ch).at(0)
+    if(caller){
+      return caller
+    }
+    if(!/\+/.test(ch)){
+      return this.ary.filter(d=>d.key==='*').at(0)
+    }
+  }  
+}
+
+
+
+/*
+var press = new Press(ed.editor)
+press.press('ctrl+s',(e)=>{
+  e.preventDefault()
+  ed.setMessage('ctrl+s')
+}).press('*',(e)=>{
+  ed.setMessage(e.key)  
+},200)
+*/      
+
+
+
+
+
+
